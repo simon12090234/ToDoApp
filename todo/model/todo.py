7afc53d7afd54aeeed34c791dcbe84a8008746a2
simon12090234 <corrasimon82@gmail.com>
2024-08-26 12:37:14 -0500

    First point: Todo class was created

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,9 @@
-# TODO: Add code here
+class Todo:
+
+    def __int__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
